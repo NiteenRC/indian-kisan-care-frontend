489cd91124261e8d50a1e6725425572c5d834436
Niteen <niteen.java@gmail.com>
2021-10-11 09:10:29 +0530

    online mock

--- src/app/_model/http-client-helper.ts
+++ src/app/_model/http-client-helper.ts
@@ -2,11 +2,14 @@ export class HttpClientHelper {
     //static baseURL: string = 'http://ec2-18-222-37-157.us-east-2.compute.amazonaws.com:8080';
 
     //Visual studio with backend
-    static baseURL = 'http://localhost:8080';
+    //static baseURL = 'http://localhost:8080';
 
     //External tomcat server
     //static baseURL: string = 'http://localhost:8080/indian-kisan-care-backend';
 
     //AWS server
     //static baseURL: string = 'http://ec2-3-133-100-17.us-east-2.compute.amazonaws.com:8080/indian-kisan-care-backend';
+
+    //Online mock api
+    static baseURL = 'https://signin.free.beeceptor.com';
 }
