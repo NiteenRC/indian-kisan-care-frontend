2f1f89e49860becf4f32acb60ae2468bf0f32394
Niteen Chougula <niteen.java@gmail.com>
2022-08-08 23:01:31 +0530

    Update http-client-helper.ts

--- src/app/_model/http-client-helper.ts
+++ src/app/_model/http-client-helper.ts
@@ -2,7 +2,7 @@ export class HttpClientHelper {
     //static baseURL: string = 'http://ec2-18-222-37-157.us-east-2.compute.amazonaws.com:8080';
 
     //Visual studio with backend
-    static baseURL = 'http://localhost:8080';
+    //static baseURL = 'http://localhost:8080';
 
     //External tomcat server
     //static baseURL: string = 'http://localhost:8080/smart-accounting-book-backend';
@@ -12,4 +12,7 @@ export class HttpClientHelper {
 
     //Online mock api
     //static baseURL = 'https://signin.free.beeceptor.com';
+    
+     //Azure
+    static baseURL = "https://smart-accounting-book.azurewebsites.net";
 }
